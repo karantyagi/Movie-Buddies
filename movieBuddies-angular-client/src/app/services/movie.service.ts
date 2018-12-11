52077820973ee2967dfbf998064f4754f82daf41
harshmeetk <36385802+harshmeetk@users.noreply.github.com>
2018-12-11 18:29:22 -0500

    Update movie.service.ts

--- movieBuddies-angular-client/src/app/services/movie.service.ts
+++ movieBuddies-angular-client/src/app/services/movie.service.ts
@@ -10,54 +10,11 @@ export class MovieService {
 
   constructor() {
     let base = 'http://localhost:5500';
-    // if (!location.toString().includes('localhost')) {
-    //   base = 'https://job-portal-server.herokuapp.com';
-    // } else {
-    //   base = 'http://localhost:5500';
-    // }
+   
     this.url = base + '/api/movie';
   }
 
-  // updateUserProfile(user) {
-  //   return fetch(this.urlUpdateProfile, {
-  //     method: 'PUT',
-  //     body: JSON.stringify(user),
-  //     credentials: 'include',
-  //     headers: {
-  //       'content-type': 'application/json'
-  //     }
-  //   }).then(response => {
-  //     if (response.headers.get('content-type') != null) {
-  //       return response.json();
-  //     } else {
-  //       return null;
-  //     }
-  //   });
-  // }
-  //
-  // deleteUser(userId) {
-  //   return fetch(this.url + '/' + userId, {
-  //     method: 'DELETE',
-  //     credentials: 'include'
-  //   });
-  // }
-  //
-  // createUser(user) {
-  //   return fetch(this.url , {
-  //     method: 'POST',
-  //     body: JSON.stringify(user),
-  //     credentials: 'include',
-  //     headers: {
-  //       'content-type': 'application/json'
-  //     }
-  //   }).then(response => {
-  //     if (response.headers.get('content-type') != null) {
-  //       return response.json();
-  //     } else {
-  //       return null;
-  //     }
-  //   });
-  // }
+  
 
   findMovieByUserId(id) {
     return fetch(this.url + '/user/' + id, {
@@ -73,7 +30,6 @@ export class MovieService {
 
 
   findAllMovies() {
-    // console.log('in here');
     return fetch(this.url, {
       credentials: 'include'
     }).then(response => {
